4a5bcd6601fe179ee87bd9ce46bb160d220148b6
SakibSibly <sibly@duck.com>
2026-01-08 10:31:34 +0600

    Update "Meta data updated"

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Nazmus Sakib Sibly - Competitive Programmer | Explorer',
-  description: 'Portfolio of Nazmus Sakib Sibly - Software Engineer, Competitive Programmer with 600+ problems solved',
-  keywords: ['Nazmus Sakib Sibly', 'competitive programmer', 'software engineer', 'web developer', 'ICPC', 'Codeforces'],
+  description: 'Portfolio of Nazmus Sakib Sibly - Motivated Learner, Competitive Programmer with 600+ problems solved',
+  keywords: ['sakibsibly', 'Nazmus Sakib Sibly', 'competitive programmer', 'Motivated Learner', 'web developer', 'ICPC', 'Codeforces', 'portfolio', 'Nazmus Sibly', 'Sakib Sibly']
 }
 
 export default function RootLayout({
